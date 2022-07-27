8a0da52e4275593524942c7e8ab1abbc387a8864
Floran Mauguin <fmauguin.pro@gmail.com>
2022-07-27 23:48:35 +0200

    setcontact no empty

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 20:16:37 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/07/27 23:30:35 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/07/27 23:46:55 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,21 @@ const Contact	*PhoneBook::getcontact(int index) const {
 	return (&this->_contacts[index]);
 }
 
-
 int	PhoneBook::getsize(void) const{
 	return (this->_size);
+}
+
+static std::string	get_field(const char *field) {
+	std::string	ret;
+
+	std::cout << field;
+	std::getline (std::cin,ret);
+	while (ret.empty())
+	{
+		std::cout << "\033[A\33[2K\r" << "Error input empty | " << field;
+		std::getline (std::cin,ret);
+	}
+	return (ret);
 }
 
 void	PhoneBook::setcontact(void) {
@@ -41,20 +53,15 @@ void	PhoneBook::setcontact(void) {
 		this->_index = 0;
 	std::cout << "\033[H\033[2J\033[3J";
 	std::cout << "Create contact" << std::endl;
-	std::cout << "First name: ";
-	std::getline (std::cin,str);
+	str = get_field("First name: ");
 	this->_contacts[this->_index].setfirstname(str);
-	std::cout << "Last name: ";
-	std::getline (std::cin,str);
+	str = get_field("Last name: ");
 	this->_contacts[this->_index].setlastname(str);
-	std::cout << "Nickname: ";
-	std::getline (std::cin,str);
+	str = get_field("Nickname: ");
 	this->_contacts[this->_index].setnickname(str);
-	std::cout << "Phone number: ";
-	std::getline (std::cin,str);
+	str = get_field("Phone number: ");
 	this->_contacts[this->_index].setphonenumber(str);
-	std::cout << "Darkest secret: ";
-	std::getline (std::cin,str);
+	str = get_field("Darkest secret: ");
 	this->_contacts[this->_index].setsecret(str);
 	this->_index++;
 	if (this->_size < 8)

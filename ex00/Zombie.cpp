1c12f26a40edda63f811d2b408326e144d678ebc
Floran Mauguin <fmauguin.pro@gmail.com>
2022-07-29 09:48:46 +0200

    zombie constructor and destructor

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
+/*   By: fmauguin <fmauguin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 13:52:32 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/07/28 14:38:41 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/07/29 09:47:25 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 #include <iostream>
 
 Zombie::Zombie(std::string name) : _name(name) {
-	std::cout << "Constructor called: " << this->_name << std::endl;
+	std::cout << "Constructor called: Zombie: " << this->_name << std::endl;
 	return ;
 }
 
 Zombie::Zombie(void) {
-	std::cout << "Constructor called" << std::endl;
+	std::cout << "Constructor called: Zombie" << std::endl;
 	return ;
 }
 
 Zombie::~Zombie(void) {
-	std::cout << "Destructor called: " << this->_name << std::endl;
+	if (this->_name.empty())
+		std::cout << "Destructor called: Zombie" << std::endl;
+	else
+		std::cout << "Destructor called: Zombie: " << this->_name << std::endl;
 	return ;
 }
 

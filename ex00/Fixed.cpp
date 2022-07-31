6162f0e4e8c955174ae35ca22c7941138d88da2b
Floran Mauguin <fmauguin.pro@gmail.com>
2022-07-31 15:39:22 +0200

    ex00 ok

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/31 15:25:35 by fmauguin          #+#    #+#             */
+/*   Updated: 2022/07/31 15:38:22 by fmauguin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _value(0) {
+	std::cout << "Default Constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(Fixed const & src) {
+	std::cout << "Copy Constructor called" << std::endl;
+	*this = src;
+
+	return ;
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+int	Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_value;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+	return ;
+}
+
+Fixed &	Fixed::operator=(Fixed const & rhs) {
+	std::cout << "Copy assignement operator called" << std::endl;
+
+	if (this != &rhs)
+		this->_value = rhs.getRawBits();
+	return *this;
+}

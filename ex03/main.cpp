ed9849a643c8435369cbb6c6c0b5c9a6a1ec5a88
Floran Mauguin <fmauguin.pro@gmail.com>
2022-08-02 09:44:35 +0200

    rendu ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 15:41:26 by fmauguin          #+#    #+#             */
-/*   Updated: 2022/07/31 23:24:15 by fmauguin         ###   ########.fr       */
+/*   Updated: 2022/08/02 09:44:06 by fmauguin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,19 @@ int main( void )
 {
 	{
 		Point	a;
-		const Fixed x(2);
+		const Fixed x(2.2f);
 		const Fixed y(3);
 		Point	b(x, y);
 		Point	c(b);
+		Point	d = c;
 
 		std::cout << std::endl << "-------------POINT CHECK-------------" <<  std::endl;
-		std::cout << "a.x: " << a.getX() << std::endl;
-		std::cout << "a.y: " << a.getX() << std::endl;
-		std::cout << "b.x: " << b.getX() << std::endl;
-		std::cout << "b.y: " << b.getY() << std::endl;
+		PRINT_POINT(a, "a");
+		PRINT_POINT(b, "b");
+		PRINT_POINT(c, "c");
+		PRINT_POINT(d, "d");
 		b = a;
-		std::cout << "b.x: " << b.getX() << std::endl;
-		std::cout << "b.y: " << b.getY() << std::endl;
-		std::cout << "c.x: " << c.getX() << std::endl;
-		std::cout << "c.y: " << c.getY() << std::endl;
-
+		PRINT_POINT(b, "b");
 	}
 	{
 		std::cout << std::endl << "--------------BSP TESTS--------------" <<  std::endl;
@@ -46,7 +43,7 @@ int main( void )
 		Point	b(0, 2);
 		Point	c(0, 0);
 		Point	pt(1, 1);
-		Point	pt1(1, 1);
+		Point	pt1(1.01f, 1.01f);
 		Point	pt2(0.5f, 0.5f);
 		Point	pt3(-0.5f, -0.5f);
 

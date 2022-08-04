9c6a444abff3dd16227f6f3368bff88c0cef28a7
Floran Mauguin <fmauguin.pro@gmail.com>
2022-08-04 13:08:57 +0200

    ex03 start

---
+++ ex03/PresidentialPardonForm.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 00:11:33 by fmauguin          #+#    #+#             */
+/*   Updated: 2022/08/04 01:07:39 by fmauguin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRESIDENTIALPARDONFORM_HPP
+#define PRESIDENTIALPARDONFORM_HPP
+
+#include <iostream>
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+private:
+	std::string _target;
+
+public:
+	PresidentialPardonForm(void);
+	PresidentialPardonForm(std::string const &target);
+	PresidentialPardonForm(PresidentialPardonForm const &src);
+	~PresidentialPardonForm(void);
+
+	PresidentialPardonForm &operator=(PresidentialPardonForm const &rhs);
+
+	std::string const &getTarget(void) const;
+	virtual void execute(Bureaucrat const &executor) const;
+};
+
+#endif
---
+++ ex03/RobotomyRequestForm.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.hpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmauguin <fmauguin@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 01:12:53 by fmauguin          #+#    #+#             */
+/*   Updated: 2022/08/04 02:19:23 by fmauguin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ROBOTOMYREQUESTFORM_HPP
+#define ROBOTOMYREQUESTFORM_HPP
+
+#include <iostream>
+#include "AForm.hpp"
+#include "Bureaucrat.hpp"
+#include <cstdlib>
+#include <ctime>
+
+class RobotomyRequestForm : public AForm
+{
+private:
+	std::string _target;
+
+public:
+	RobotomyRequestForm(void);
+	RobotomyRequestForm(std::string const &target);
+	RobotomyRequestForm(RobotomyRequestForm const &src);
+	~RobotomyRequestForm(void);
+
+	RobotomyRequestForm &operator=(RobotomyRequestForm const &rhs);
+
+	std::string const &getTarget(void) const;
+	virtual void execute(Bureaucrat const &executor) const;
+};
+
+#endif
